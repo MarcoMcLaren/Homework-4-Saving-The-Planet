a29e3a5130ad47f60cc90fc49ff63aefcbbc07f7
Marco Mac <mclarenmarco998@gmail.com>
2022-08-11 13:10:17 +0200

    Nog charts in gesit

--- HW4.2/Controllers/Globals.cs
+++ HW4.2/Controllers/Globals.cs
@@ -9,11 +9,11 @@ namespace HW4._2.Controllers
     public static class Globals
     {
         //Establish connection to database
-        //public static string ConnectionString = "Data Source=MARCO_LAPTOP;Initial Catalog=SaveTheWorld;Integrated Security=True";
+        public static string ConnectionString = "Data Source=MARCO_LAPTOP;Initial Catalog=SaveTheWorld;Integrated Security=True";
         //Please note this is connected to MARCO_LAPTOP, if you wish to run it on your own computer you will have to change the path
 
         //PC connection
-        public static string ConnectionString = "Data Source=MARCO_PC;Initial Catalog=SaveTheWorld;Integrated Security=True";
+        //public static string ConnectionString = "Data Source=MARCO_PC;Initial Catalog=SaveTheWorld;Integrated Security=True";
 
         //Create all the lists from their respective models
         public static List<Paper> PaperList = new List<Paper>();
